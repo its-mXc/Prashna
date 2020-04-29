8cb60e4332acbbcd31f5af8dfcb3dacbf183fe1f
Uday Nayak <udaynayakgkv@gmail.com>
2020-04-29 10:58:37 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   get 'forgot-password', to: "users#forgot_password"
   post 'reset-password', to: "users#reset_password"
+  get 'signup', to: "users#new"
   
   Rails.application.routes.default_url_options[:host] = "prashna"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
