7e9ecee108a7f2f897cc4a1bce3e68febb09e452
Uday Nayak <udaynayakgkv@gmail.com>
2020-05-05 18:16:43 +0530

    hello

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,32 +1,46 @@
-$(function() {
-  function split( val ) {
-    return val.split( /,\s*/ );
+function split( val ) {
+  return val.split( /,\s*/ );
+}
+
+function extractLast( term ) {
+  return split( term ).pop();
+}
+
+class AutoCompleteInput {
+  constructor(options) {
+    this.inputElement = options["inputElement"]
   }
 
-  function extractLast( term ) {
-    return split( term ).pop();
+  init() {
+    this.inputElement.autocomplete({
+      source: function( request, response ) {
+                $.getJSON( "/topics", {
+                  term: extractLast( request.term )
+                }, response );
+              },
+      select: function( event, ui ) {
+                // Add the selected term appending to the current values with a comma
+                var terms = split( this.value );
+                // remove the current input
+                terms.pop();
+                // add the selected item
+                terms.push( ui.item.value );
+                // join all terms with a comma
+                this.value = terms.join( ", " );
+                return false;
+              },
+      focus: function() {
+               // prevent value inserted on focus when navigating the drop down list
+               return false;
+             }
+    });
   }
+}
 
-  $( "#tag-autocomplete" ).autocomplete({
-    source: function( request, response ) {
-              $.getJSON( "/topics", {
-                term: extractLast( request.term )
-              }, response );
-            },
-    select: function( event, ui ) {
-              // Add the selected term appending to the current values with a comma
-              var terms = split( this.value );
-              // remove the current input
-              terms.pop();
-              // add the selected item
-              terms.push( ui.item.value );
-              // join all terms with a comma
-              this.value = terms.join( ", " );
-              return false;
-            },
-    focus: function() {
-             // prevent value inserted on focus when navigating the drop down list
-             return false;
-           }
-  });
-});
+
+let options = {
+  inputElement: $( "#tag-autocomplete" ),
+}
+
+let autoCompleteInput = new AutoCompleteInput(options)
+autoCompleteInput.init()
