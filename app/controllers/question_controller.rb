baef1f54cc2e9ec4d24b7b0dcf10ce1a9f648665
Uday Nayak <udaynayakgkv@gmail.com>
2020-05-06 18:08:18 +0530

    notification channel

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -6,23 +6,26 @@ class QuestionController < ApplicationController
 
   def create
     @question = current_user.questions.new(question_params)
-    if params[:commit] = "draft"
+    if params[:commit] == "draft"
       @question.status = 0
     else
       @question.status = 1
     end
     respond_to do |format|
-      if @question.published?
-        @question.topics.each do |topic|
-          topic.users.each do |user|
-            puts user
-            notification = user.notifications.new
-            notification.question = @question
-            notification.save
+      
+      if @question.save
+        if @question.published?
+          @question.topics.each do |topic|
+            topic.users.each do |user|
+              puts user
+              notification = user.notifications.new
+              notification.question = @question
+              notification.save
+            end
           end
         end
-      end
-      if @question.save
+        @notifications = current_user.notifications
+        ActionCable.server.broadcast 'notifications', html: render_to_string('notifications/index', layout: false)
           format.html { redirect_to my_profile_path, notice: "Question posted" }
         # format.json { render :show, status: :created, location: @user }
       else
