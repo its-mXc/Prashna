cf81efd452923069b4cda7f4e4f8a9efa589d3a9
akhil <akhil@vinsol.com>
2020-05-06 16:47:32 +0530

    Added CR comments

--- app/controllers/topic_controller.rb
+++ app/controllers/topic_controller.rb
@@ -1,7 +1,10 @@
 class TopicController < ApplicationController
 
+  #FIXME_AB: you should check that user is logged in
+
   def index
+    #FIXME_AB: find unique so that you don't need to do it while making json
     @topics = Topic.search(params[:term])
-    render json: @topics.map(&:name).uniq 
+    render json: @topics.map(&:name).uniq
   end
 end
