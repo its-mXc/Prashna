b5a8f26e8fcf933b333bd5c2db048c14d815b0d3
Uday Nayak <udaynayakgkv@gmail.com>
2020-05-29 15:43:50 +0530

    x number of questions

--- app/controllers/api/topics_controller.rb
+++ app/controllers/api/topics_controller.rb
@@ -3,7 +3,7 @@ module Api
     def show
       topic = Topic.find_by_name(params[:id])
       if topic
-        @questions = topic.questions.published
+        @questions = topic.questions.published.recent.limit(ENV["topic_api_question_amount"].to_i)
       else
         render json: {error: "No such topic exist"}
       end
