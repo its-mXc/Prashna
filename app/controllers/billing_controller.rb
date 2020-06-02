4769c71c0a2dd8ff146b9bb0d1e3830ca18e097b
Uday Nayak <udaynayakgkv@gmail.com>
2020-06-02 13:53:15 +0530

    improved

--- app/controllers/billing_controller.rb
+++ app/controllers/billing_controller.rb
@@ -11,24 +11,28 @@ class BillingController < ApplicationController
   end
 
   def create_card 
+    logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
     if current_user.stripe_id.nil?
       customer = Stripe::Customer.create({"email": current_user.email}) 
+      logger.tagged('Stripe Customer Created') { logger.info customer }  
       #here we are creating a stripe customer with the help of the Stripe library and pass as parameter email. 
-      current_user.update(:stripe_id => customer.id)
+      current_user.update(stripe_id: customer.id)
       #we are updating current_user and giving to it stripe_id which is equal to id of customer on Stripe
     end
-
+    
     card_token = params[:stripeToken]
     #it's the stripeToken that we added in the hidden input
     if card_token.nil?
       redirect_to billing_index_path, notice: "Error, no card token"
     end
     #checking if a card was giving.
-
+    
     customer = Stripe::Customer.new current_user.stripe_id
     customer.source = card_token
     #we're attaching the card to the stripe customer
     customer.save
+    logger.tagged('Stripe Customer Card Added') { logger.info customer }  
+
 
     redirect_to my_profile_path, notice: "Card Added"
   end
