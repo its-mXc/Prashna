fb3a0238a620d042365ae51d7cec6699ed314e15
Uday Nayak <udaynayakgkv@gmail.com>
2020-06-04 18:20:03 +0530

    fixes

--- app/assets/javascripts/classes/autoLoad.js
+++ app/assets/javascripts/classes/autoLoad.js
@@ -8,13 +8,13 @@ class AutoLoad {
     this.persistentElement.hide();
 
     this.persistentElement.parent('form').on('ajax:success', function(event, data, type){
+      $('.question-notification').children().remove()
       if(data.new_questions_size) {
         let paragraph = $('<p />').addClass("modal-open").text(`${data.new_questions_size} new Questions posted`)
         let modal = $('<div />').addClass('container alert alert-dismissible alert-success').append($('<button />').addClass('close').attr('type', 'buuton').data('dismiss', 'alert').text('x')).append(paragraph)
         paragraph.on('click', function() {
           window.location.reload()
         })
-        $('.question-notification').children().remove()
         $('.question-notification').append(modal)
       }
 
